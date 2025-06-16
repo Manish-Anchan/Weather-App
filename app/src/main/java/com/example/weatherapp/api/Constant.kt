402a4fc7f7ed6152001f0aa59f7f957ef5b31eaf
Manish-Anchan <manishanchan789@gmail.com>
2025-06-16 17:30:24 +0530

    Completed

---
+++ app/src/main/java/com/example/weatherapp/api/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp.api
+
+object Constant {
+    const val APIKEY = "66922e8ee08c422cbb0123910251506"
+}
---
+++ app/src/main/java/com/example/weatherapp/api/WeatherAppModel.kt
@@ -0,0 +1,6 @@
+package com.example.weatherapp.api
+
+data class WeatherAppModel(
+    val current: Current,
+    val location: Location
+)
